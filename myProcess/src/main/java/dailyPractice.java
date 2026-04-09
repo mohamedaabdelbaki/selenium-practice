4e78aa68c5991d050d832222019b25d39bf0e65e
mohamedaabdelbaki <mohamedaabdelbaki6@gmail.com>
2026-04-09 17:58:52 +0200

    Problem 2 — If condition

--- myProcess/src/main/java/dailyPractice.java
+++ myProcess/src/main/java/dailyPractice.java
@@ -6,6 +6,7 @@ public class dailyPractice {
 
     static void main()
     {
+ /*
         Scanner obj=new Scanner(System.in);
 
         System.out.println("Enter first number");
@@ -18,6 +19,25 @@ public class dailyPractice {
         System.out.println("Subtract ="+(firstNumber-secondNumber));
         System.out.println("Multiplication ="+(firstNumber*secondNumber));
         System.out.println("Divide ="+(float)(firstNumber/secondNumber));
+*/
+
+//2nd problem
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter random number");
+        double num= obj.nextDouble();
+
+        if (num==0)
+        {
+            System.out.println("Zero");
+        } else if (num>0) {
+
+            System.out.println("number is Positive");
+        }
+
+        else
+        {
+            System.out.println("number is Negative");
+        }
 
 
     }
