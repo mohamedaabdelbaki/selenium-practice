d17675ce6cb5fb32609793dc874d9c39dbf7af09
mohamedaabdelbaki <mohamedaabdelbaki6@gmail.com>
2026-04-09 18:25:24 +0200

    add Problem 3 — Loops + Condition

--- myProcess/src/main/java/dailyPractice.java
+++ myProcess/src/main/java/dailyPractice.java
@@ -22,6 +22,8 @@ public class dailyPractice {
 */
 
 //2nd problem
+
+/*
         Scanner obj=new Scanner(System.in);
         System.out.println("Enter random number");
         double num= obj.nextDouble();
@@ -38,7 +40,46 @@ public class dailyPractice {
         {
             System.out.println("number is Negative");
         }
+*/
+
+ //Problem 3 — Loops + Condition
+/*
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter 5 numbers");
+
+        double[] numbers=new double[5];
+        int positiveCount=0;
+        int negativeCount=0;
+        int zeroCount=0;
 
 
+        for(int i=0 ;i<=numbers.length-1;i++)
+        {
+            System.out.println("Enter "+(i+1)+" number");
+            numbers[i]=obj.nextDouble();
+
+
+            if(numbers[i]>0)
+            {
+                positiveCount+=1;
+
+            } else if (numbers[i]<0)
+            {
+                negativeCount+=1;
+            }
+            else
+            {
+                zeroCount+=1;
+
+            }
+        }
+
+        System.out.println("number of element in the array "+numbers.length);
+        System.out.println("count of Positive number "+positiveCount);
+        System.out.println("count of Negative number"+negativeCount);
+        System.out.println("count of zeros in array"+zeroCount);
+
+
+*/
     }
 }
